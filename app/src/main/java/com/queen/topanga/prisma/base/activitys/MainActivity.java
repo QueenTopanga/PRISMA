17e0c56d569d2303dbee4b5f1308a67e14a8d50d
QueenTopanga <luismiguelam89@gmail.com>
2018-04-18 16:23:45 +0200

    Segunda prueba

---
+++ app/src/main/java/com/queen/topanga/prisma/base/activitys/MainActivity.java
@@ -0,0 +1,49 @@
+package com.queen.topanga.prisma.base.activitys;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.queen.topanga.prisma.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    //BOTONES MENU---------------------------------------------------------------
+    public void opencollection(View view) {
+        Intent show = new Intent(this, CollectionActivity.class);
+        startActivity(show);
+    }
+
+    public void openmissions(View view) {
+        Intent show = new Intent(this, MissionsActivity.class);
+        startActivity(show);
+    }
+
+    public void openmain(View view) {
+        Intent show = new Intent(this, MainActivity.class);
+        startActivity(show);
+    }
+
+    public void openshop(View view) {
+        Intent show = new Intent(this, ShopActivity.class);
+        startActivity(show);
+    }
+
+    public void opensettings(View view) {
+        Intent show = new Intent(this, SettingsActivity.class);
+        startActivity(show);
+    }
+
+    public void openmode(View view) {
+        Intent show = new Intent(this, ModeActivity.class);
+        startActivity(show);
+    }
+}
